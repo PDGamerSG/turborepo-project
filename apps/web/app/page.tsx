05700971448c5ec38361495bfcbee0d09adc09a0
Pallab Das <pallab4586das@gmail.com>
2026-03-06 19:39:56 +0530

    Update page.tsx

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,14 +1,16 @@
 import { client } from "@repo/db/client";
 
 export default async function Home() {
-  const user = await client.user.findFirst();
+  // const user = await client.user.findFirst();
 
   console.log(user); // check server logs
 
   return (
     <div>
+          name:
           <p>{user?.username}</p>
           <p>{user?.password}</p>
+          password:
     </div>
   );
 }
